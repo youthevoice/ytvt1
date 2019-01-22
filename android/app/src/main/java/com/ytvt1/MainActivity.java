f95693f94f17e6567c9b6aff901f373ce8f357ca
JEEVAN KUMAR <jeevan@JEEVANs-MacBook-Pro-2.local>
2019-01-22 18:14:29 +0530

    splash screen

--- android/app/src/main/java/com/ytvt1/MainActivity.java
+++ android/app/src/main/java/com/ytvt1/MainActivity.java
@@ -1,6 +1,8 @@
 package com.ytvt1;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
@@ -8,6 +10,18 @@ public class MainActivity extends ReactActivity {
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
      */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // SplashScreen.show(this);  // here
+        if (android.os.Build.VERSION.SDK_INT >= 21) {
+            getWindow().setNavigationBarColor(getResources().getColor(R.color.navigationBarColor));
+        }
+        SplashScreen.show(this, R.style.SplashScreenTheme);
+
+        super.onCreate(savedInstanceState);
+    }
+
     @Override
     protected String getMainComponentName() {
         return "ytvt1";
