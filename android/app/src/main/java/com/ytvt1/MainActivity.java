9c89aaa7530e0b8069bb840f85f4c7c4da1c9496
JEEVAN KUMAR <jeevan@JEEVANs-MacBook-Pro-2.local>
2019-01-23 12:20:43 +0530

    added orientation
    Added Orientation

--- android/app/src/main/java/com/ytvt1/MainActivity.java
+++ android/app/src/main/java/com/ytvt1/MainActivity.java
@@ -3,6 +3,13 @@ package com.ytvt1;
 import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import org.devio.rn.splashscreen.SplashScreen;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+import android.content.Intent; // <--- import
+import android.content.res.Configuration; // <--- import
+
+
 
 public class MainActivity extends ReactActivity {
 
@@ -23,7 +30,27 @@ public class MainActivity extends ReactActivity {
     }
 
     @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        Intent intent = new Intent("onConfigurationChanged");
+        intent.putExtra("newConfig", newConfig);
+        this.sendBroadcast(intent);
+    }
+
+    @Override
     protected String getMainComponentName() {
         return "ytvt1";
     }
+
+    @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+            return new ReactActivityDelegate(this, getMainComponentName()) {
+      @Override
+      protected ReactRootView createRootView() {
+                       return new RNGestureHandlerEnabledRootView(MainActivity.this);
+                      }
+    };
+          }
+
+
 }
--- components/App.js
+++ components/App.js
@@ -10,11 +10,14 @@ import React, { Component } from "react";
 import { Platform, StyleSheet, Text, View } from "react-native";
 import SplashScreen from "react-native-splash-screen";
 
+import Orientation from "react-native-orientation";
+
 export default class App extends Component {
   componentDidMount() {
     // do stuff while splash screen is shown
     // After having done stuff (such as async tasks) hide the splash screen
     SplashScreen.hide();
+    Orientation.lockToPortrait();
   }
 
   render() {
